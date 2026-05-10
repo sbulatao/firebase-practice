913d24e9f8d5d5f982c637743873a1e5e5e21e1e
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-05-10 11:53:13 -0700

    practicing firebase: register, login, logout, and onAuthStateChanges

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { auth } from './firebase/init';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      // console.log(user.email[0].toUpperCase());
+      if (user) {
+        setUser(user);
+      }
+    })
+  }, [])
+
+  function register(){
+    // console.log('register');
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'password')
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  function login(){
+    signInWithEmailAndPassword(auth, 'email@email.com', 'password')
+    .then((user) => {
+      console.log(user);
+      setUser(user);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    })
+  }
+
+  function logout(){
+    signOut(auth);
+    setUser({});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'loading...': user.email}
     </div>
   );
 }
