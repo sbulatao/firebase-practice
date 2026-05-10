f7a0d2e62cb21056b4af504825628ad308692ff9
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-05-10 12:19:57 -0700

    Added .env file for config info

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAXj-X0o_VxEgGXNTDVDFT7l7eEQsmTlOc",
-  authDomain: "fir-practice-dc426.firebaseapp.com",
-  projectId: "fir-practice-dc426",
-  storageBucket: "fir-practice-dc426.firebasestorage.app",
-  messagingSenderId: "189337329322",
-  appId: "1:189337329322:web:00425d2b170cc74adc3ad3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
